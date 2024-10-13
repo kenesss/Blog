695e7c923cba36dec814c8d631f043c2933b6982
kenesss <220103140@stu.sdu.edu.kz>
2024-10-13 23:33:10 +0500

    first

---
+++ server/auth/controller.js
@@ -0,0 +1,48 @@
+const User = require('./User')
+const bcrypt = require('bcrypt')
+
+const signUp = async(req, res) => {
+    if (
+      req.body.email.length <= 0 &&
+      req.body.full_name.length <= 0 &&
+      req.body.password.length <= 0 &&
+      req.body.re_password.length <= 0
+    ) {
+        res.redirect('/regester?error=1')
+    } else if(req.body.password !== req.body.re_password){
+        res.redirect('/regester?error=2')
+    }
+    const findUser = await User.findOne({email: req.body.email}).count()
+    if(findUser){
+        res.redirect("/regester?error=3");
+    }
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(req.body.password, salt, function (err, hash) {
+        new User({
+          email: req.body.email,
+          full_name: req.body.full_name,
+          password: hash,
+        }).save();
+        res.redirect("/login");
+      });
+    });
+}
+
+const signIn = (req, res) => {
+  res.redirect(`/profile/${req.user._id}`);
+};
+
+const singOut = (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+  res.redirect("/");
+};
+
+module.exports = {
+  signUp,
+  signIn,
+  singOut,
+};
